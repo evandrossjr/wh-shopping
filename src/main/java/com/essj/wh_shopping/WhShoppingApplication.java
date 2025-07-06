cf8e20488509ab1007063cfad650fca5860d02b9
Evandro Sacramento II <evandro-dev@outlook.com>
2025-07-06 12:46:58 -0300

    primeiro commit

---
+++ src/main/java/com/essj/wh_shopping/WhShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.essj.wh_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/essj/wh_shopping/WhShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essj.wh_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
