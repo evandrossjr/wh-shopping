505a09235994a539b8b5c61d6c29dd3e3f4d6e8b
Evandro Sacramento II <evandro-dev@outlook.com>
2025-07-07 23:34:04 -0300

    Criação da interface ShopRepository com métodos extras.

---
+++ src/main/java/com/essj/wh_shopping/repositories/ShopRepository.java
@@ -0,0 +1,16 @@
+package com.essj.wh_shopping.repositories;
+
+import com.essj.wh_shopping.entities.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface ShopRepository extends JpaRepository<Shop, Long>{
+
+    public List<Shop> findByUserIndentifier(String userIdentifier);
+
+    public List<Shop> findAllByTotalGreaterThan(Float total);
+
+    List<Shop> findAllByDateGreaterThanEqualçs(Date date);
+}
