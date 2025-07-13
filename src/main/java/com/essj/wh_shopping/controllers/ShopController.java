842cfa9612928a5d2a4e329f901d0ca1fe71dee0
Evandro Sacramento II <evandro-dev@outlook.com>
2025-07-13 20:13:54 -0300

    Novas rotas no ShopController através dos métodos getShopsByFilter, getReportByDate

--- src/main/java/com/essj/wh_shopping/controllers/ShopController.java
+++ src/main/java/com/essj/wh_shopping/controllers/ShopController.java
@@ -2,14 +2,18 @@ package com.essj.wh_shopping.controllers;
 
 
 import com.essj.wh_shopping.DTO.ShopDTO;
+import com.essj.wh_shopping.DTO.ShopReportDTO;
 import com.essj.wh_shopping.entities.Shop;
+import com.essj.wh_shopping.services.ReportService;
 import com.essj.wh_shopping.services.ShopService;
 import jakarta.validation.Valid;
 import org.aspectj.lang.annotation.DeclareWarning;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -18,12 +22,16 @@ public class ShopController {
     @Autowired
     private ShopService shopService;
 
+    @Autowired
+    private ReportService reportService;
+
 
     @GetMapping("/shopping")
     public List<ShopDTO> getShops(){
         List<ShopDTO> produtos = shopService.findAll();
         return produtos;
     }
+
 
     @GetMapping("/shopping/shopByUser/{userIdentifier}")
     public List<ShopDTO> getShops(@PathVariable String userIdentifier){
@@ -46,5 +54,27 @@ public class ShopController {
     public ShopDTO newShop(@Valid @RequestBody ShopDTO shopDTO){
         return shopService.save(shopDTO);
     }
+
+    @GetMapping("/shopping/search")
+    public List<ShopDTO> getShopsByFilter(
+            @RequestParam(name = "dataInicio", required = true)
+            @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataInicio,
+            @RequestParam(name = "dataFim", required = false)
+            @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataFim,
+            @RequestParam(name = "valorMinimo", required = false)
+            Float valorMinimo) {
+        return reportService.getShopByFilter(dataInicio, dataFim, valorMinimo);
+    }
+
+    @GetMapping("shopping/report")
+    public ShopReportDTO getReportByDate(
+            @RequestParam(name = "dataInicio", required = true)
+            @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataInicio,
+            @RequestParam(name = "dataFim", required = true)
+            @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataFim){
+        return reportService.getReportByDate(dataInicio, dataFim);
+    }
+
+
 }
 
